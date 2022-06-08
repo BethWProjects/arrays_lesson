d90a64d30f4ff4ae39a9c0ec217fbf857f4f207a
Beth Wilson <bethwilson777@gmail.com>
2022-06-08 11:03:16 -0600

    Added comments

--- arrays.js
+++ arrays.js
@@ -7,3 +7,4 @@ console.log(friends["Jake"]);
 console.log(ages.slice(2));
 //remove and return the last element "False"
 console.log(meetsRequirement.pop);
+// The default index position for Arrays is 0.  So the the first position in the array would start at 0, and count up from 1.  For some methods, the index will begin at a starting point, or ending point, so keeping that in mind is important when calling methods.  If an array has three elements, the index numbers for that array would be 0, 1, 2
