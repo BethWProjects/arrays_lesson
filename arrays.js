779dd2179b779ca64928903724cd5aeed6f3db58
Beth Wilson <bethwilson777@gmail.com>
2022-06-08 10:59:15 -0600

    New methods with explanations

--- arrays.js
+++ arrays.js
@@ -1,3 +1,9 @@
 var friends = ["Jake", "Kate", "Logan", "Wyatt"];
 var ages = [40, 17, 19, 14];
 var meetsRequirement = [True, False, False, False];
+//print Jake
+console.log(friends["Jake"]);
+// remove 40 and 17
+console.log(ages.slice(2));
+//remove and return the last element "False"
+console.log(meetsRequirement.pop);
